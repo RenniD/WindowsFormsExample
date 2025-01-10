962e246acc9a973147efdfa79f3d4def6cc11eee
Denis Shevchenko <Denis Shevchenko@DESKTOP-K7IOPTV>
2025-01-10 21:14:28 +0200

    Add project files.

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.checkBoxStart = new System.Windows.Forms.CheckBox();
+            this.textLogin = new System.Windows.Forms.TextBox();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textPassword2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.labelErrorMessage = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Black", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(317, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Registration";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(433, 304);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 1;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonEnter.FlatAppearance.BorderSize = 0;
+            this.buttonEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEnter.Location = new System.Drawing.Point(322, 348);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(133, 51);
+            this.buttonEnter.TabIndex = 2;
+            this.buttonEnter.Text = "Registr";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // checkBoxStart
+            // 
+            this.checkBoxStart.AutoSize = true;
+            this.checkBoxStart.Location = new System.Drawing.Point(248, 286);
+            this.checkBoxStart.Name = "checkBoxStart";
+            this.checkBoxStart.Size = new System.Drawing.Size(228, 17);
+            this.checkBoxStart.TabIndex = 4;
+            this.checkBoxStart.Text = "Приймаю умови полоєення бла-бла-бла";
+            this.checkBoxStart.UseVisualStyleBackColor = true;
+            // 
+            // textLogin
+            // 
+            this.textLogin.Location = new System.Drawing.Point(341, 110);
+            this.textLogin.Multiline = true;
+            this.textLogin.Name = "textLogin";
+            this.textLogin.Size = new System.Drawing.Size(181, 33);
+            this.textLogin.TabIndex = 5;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLogin.Location = new System.Drawing.Point(122, 110);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(79, 29);
+            this.labelLogin.TabIndex = 6;
+            this.labelLogin.Text = "Login:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(122, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password:";
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(341, 149);
+            this.textPassword.Multiline = true;
+            this.textPassword.Name = "textPassword";
+            this.textPassword.PasswordChar = '*';
+            this.textPassword.Size = new System.Drawing.Size(181, 33);
+            this.textPassword.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(122, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(216, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Confirm Password:";
+            // 
+            // textPassword2
+            // 
+            this.textPassword2.Location = new System.Drawing.Point(341, 188);
+            this.textPassword2.Multiline = true;
+            this.textPassword2.Name = "textPassword2";
+            this.textPassword2.PasswordChar = '*';
+            this.textPassword2.Size = new System.Drawing.Size(181, 33);
+            this.textPassword2.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(122, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 29);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "E-mail:";
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(341, 228);
+            this.textEmail.Multiline = true;
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(181, 33);
+            this.textEmail.TabIndex = 11;
+            // 
+            // labelErrorMessage
+            // 
+            this.labelErrorMessage.AutoSize = true;
+            this.labelErrorMessage.Location = new System.Drawing.Point(371, 329);
+            this.labelErrorMessage.Name = "labelErrorMessage";
+            this.labelErrorMessage.Size = new System.Drawing.Size(0, 13);
+            this.labelErrorMessage.TabIndex = 13;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(568, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labelErrorMessage);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textPassword2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.labelLogin);
+            this.Controls.Add(this.textLogin);
+            this.Controls.Add(this.checkBoxStart);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.CheckBox checkBoxStart;
+        private System.Windows.Forms.TextBox textLogin;
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textPassword2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.Label labelErrorMessage;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            string username = textLogin.Text;
+            string password = textPassword.Text;
+            string confirmPassword = textPassword2.Text;
+            string email = textEmail.Text;
+            bool isTermsAccepted = checkBoxStart.Checked;
+
+
+            string login_user = "dsadas";
+            string password_user = "134";
+
+            // перевірка паролів 
+            if (password != confirmPassword)
+            {
+                labelErrorMessage.Text = "Паролі не співпадають";
+                labelErrorMessage.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+            // перевірка введення данних
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword) || string.IsNullOrEmpty(email))
+            {
+                labelErrorMessage.Text = "Всі поля повинні бути заповнені!";
+                labelErrorMessage.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+            // чи приймають умови
+            if (!isTermsAccepted)
+            {
+                labelErrorMessage.Text = "Умови повинні бути прийняті!";
+                labelErrorMessage.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+            // всі перевірки успішні
+            labelErrorMessage.Text = "Реєстація успішна!";
+            labelErrorMessage.ForeColor = System.Drawing.Color.Green;
+
+
+            // очищення полів 
+            textLogin.Clear();
+            textPassword.Clear();
+            textPassword2.Clear();
+            textEmail.Clear();
+
+            checkBoxStart.Checked = false;
+
+            System.Diagnostics.Process.Start("");
+
+        }
+
+
+    }
+}
